38c6fba9d75ceacc5cf0cde422b7798973485241
anthonycarl <smeat32@yahoo.com>
2014-08-12 21:53:20 -0400

    break out unit test class. Add another test case

--- MatasanoCryptoChallenges.UnitTests/Set1/Challenge1Tests.cs
+++ MatasanoCryptoChallenges.UnitTests/Set1/Challenge1Tests.cs
@@ -23,14 +23,4 @@ namespace MatasanoCryptoChallenges.UnitTests.Set1
             Assert.Throws<ArgumentNullException>(() => Challenge1.ByteHexToBase64(null));
         }
     }
-
-    public class Challenge2Test
-    {
-        [Theory]
-        [InlineData("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965", "746865206b696420646f6e277420706c6179")]
-        public void HexXor_ValidData_ValidReturn(string hexA, string hexB, string hexResult)
-        {
-            Assert.Equal(hexResult, Challenge2.HexXor(hexA, hexB));
-        }
-    }
 }
---
+++ MatasanoCryptoChallenges.UnitTests/Set1/Challenge2Test.cs
@@ -0,0 +1,17 @@
+﻿using MatasanoCryptoChallenges.Set1;
+using Xunit;
+using Xunit.Extensions;
+
+namespace MatasanoCryptoChallenges.UnitTests.Set1
+{
+    public class Challenge2Test
+    {
+        [Theory]
+        [InlineData("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965", "746865206b696420646f6e277420706c6179")]
+        [InlineData("","","")]
+        public void HexXor_ValidData_ValidReturn(string hexA, string hexB, string hexResult)
+        {
+            Assert.Equal(hexResult, Challenge2.HexXor(hexA, hexB));
+        }
+    }
+}
